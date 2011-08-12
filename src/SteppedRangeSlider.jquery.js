26e6a1aaeeac4551e885e0a3aebb72d157169369
Blake Simpson <simpson@ubilabs.net>
2011-08-12 16:05:39 +0200

    Refactor and comment SteppedRangedSlider class

--- src/SteppedRangeSlider.jquery.js
+++ src/SteppedRangeSlider.jquery.js
@@ -22,115 +22,116 @@
 **/
 
 (function($){
-   var SteppedRangeSlider = function(element, options)
-   {
-       var $elem = $(element);
-       var $obj = this;
-       // default options
-       var $opts = {
-         divider: " - "
-       }
-       
-       //add the custom options to the $opts hash
-       $.extend($opts, options || {});
-        
-       //basic slider requirements
-       $opts.min =    options.min    || $opts.slider_values[0];
-       $opts.max =    options.max    || $opts.slider_values[$opts.slider_values.length-1];
-       $opts.values = options.values || [$opts.min, $opts.max];
-
-       this.displaySliderValue = function() {
-          // get slider values
-          var value_one = $obj.slider.slider('values', 0);
-          var value_two = $obj.slider.slider('values', 1);
-          //set the display text, only if the display text option is available
-          if($opts.display_area_class)
-          {
-            $('.' + $opts.display_area_class).text($obj.getDisplayMask(value_one) + $opts.divider + $obj.getDisplayMask(value_two));
+  //SteppedRangeSlider class
+  var SteppedRangeSlider = function(element, options)
+  {
+    var $elem = $(element);
+    var $obj = this;
+    // default options
+    var $opts = {
+      divider: " - "
+    }
+    
+    //add the custom options to the $opts hash
+    $.extend($opts, options || {});
+     
+    //basic slider requirements
+    $opts.min =    options.min    || $opts.slider_values[0];
+    $opts.max =    options.max    || $opts.slider_values[$opts.slider_values.length-1];
+    $opts.values = options.values || [$opts.min, $opts.max];
+    
+    this.displaySliderValue = function() {
+      // get slider values
+      var slider_value_one = $obj.slider.slider('values', 0);
+      var slider_value_two = $obj.slider.slider('values', 1);
+      //set the display text, only if the display text option is available
+      if($opts.display_area_class)
+      {
+        $('.' + $opts.display_area_class).text($obj.getDisplayMask(slider_value_one) + $opts.divider + $obj.getDisplayMask(slider_value_two));
+      }
+      //update hidden fields, if they are available
+      if($opts.hidden_fields)
+      {
+        $("#" + $opts.hidden_fields[0]).val($obj.getRealValue(slider_value_one));
+        $("#" + $opts.hidden_fields[1]).val($obj.getRealValue(slider_value_two)); 
+      }
+    }
+    
+    //credit to: Alconja - http://stackoverflow.com/questions/967372/jquery-slider-how-to-make-step-size-change for inital idea
+    this.findNearest = function(value) {
+      var nearest = null;
+      var offset = null;
+      
+      for (var i = 0; i < $opts.slider_values.length; i++) {
+        if (($opts.include_left && $opts.slider_values[i] <= value) || ($opts.include_right && $opts.slider_values[i] >= value)) {
+          var new_offset = Math.abs(value - $opts.slider_values[i]);
+          if (offset == null || new_offset < offset) {
+            nearest = $opts.slider_values[i];
+            offset = new_offset;
           }
-          //update hidden fields, if they are available
-          if($opts.hidden_fields)
-          {
-            $("#" + $opts.hidden_fields[0]).val($obj.getRealValue(value_one));
-            $("#" + $opts.hidden_fields[1]).val($obj.getRealValue(value_two)); 
+        }
+      }
+      return nearest;
+    }
+    
+    this.getRealValue = function(sliderValue) {
+      for (var i = 0; i < $opts.slider_values.length; i++) {
+        if ($opts.slider_values[i] >= sliderValue) {
+          return $opts.true_values[i];
+        }
+      }
+      return 0;
+    }
+    
+    this.getDisplayMask = function(sliderValue) {
+      for (var i = 0; i < $opts.slider_values.length; i++) {
+        if ($opts.slider_values[i] >= sliderValue) {
+          // check if a display_unit has been given
+          if(typeof $opts.display_unit != "undefined") {
+            //find the true value and add check if the display_unit_placement has been set to "after", if not default to "before"
+            var true_value = $opts.true_values[i];
+            return $opts.display_unit_place && $opts.display_unit_place == "after" ? true_value + $opts.display_unit : $opts.display_unit + true_value;
+          } else if(typeof $opts.display_masks != "undefined" && $opts.display_masks.length > 0) {
+            // find the display_mask values, or return a zero
+            return $opts.display_masks[i] || 0;
+          } else {
+            //if no masks or unit values given, just show the display value
+            return $obj.getRealValue(sliderValue);
           }
-       }
-       
-       //credit to: Alconja - http://stackoverflow.com/questions/967372/jquery-slider-how-to-make-step-size-change
-       this.findNearest = function(value) {
-           var nearest = null;
-           var diff = null;
-
-           for (var i = 0; i < $opts.slider_values.length; i++) {
-               if (($opts.include_left && $opts.slider_values[i] <= value) || ($opts.include_right && $opts.slider_values[i] >= value)) {
-                   var newDiff = Math.abs(value - $opts.slider_values[i]);
-                   if (diff == null || newDiff < diff) {
-                       nearest = $opts.slider_values[i];
-                       diff = newDiff;
-                   }
-               }
-           }
-           return nearest;
-       }
-       
-       this.getRealValue = function(sliderValue) {
-           for (var i = 0; i < $opts.slider_values.length; i++) {
-               if ($opts.slider_values[i] >= sliderValue) {
-                   return $opts.true_values[i];
-               }
-           }
-           return 0;
-       }
-       
-       this.getDisplayMask = function(sliderValue) {
-         for (var i = 0; i < $opts.slider_values.length; i++) 
-         {
-           if ($opts.slider_values[i] >= sliderValue) 
-           {
-             if(typeof $opts.display_unit != "undefined")
-             {
-               var true_value = $opts.true_values[i];
-               return $opts.display_unit_place && $opts.display_unit_place == "after" ? true_value + $opts.display_unit : $opts.display_unit + true_value;
-             } 
-             else if(typeof $opts.display_masks != "undefined" && $opts.display_masks.length > 0) 
-             {
-                   return $opts.display_masks[i] || 0;
-             } 
-             else 
-             {
-               //if no masks or unit values given, just show the display value
-               return $obj.getRealValue(sliderValue);
-             }
-           }
-         }
-       }
-       
-       this.initSlider = function() {
-         $obj.slider = $elem.slider({
-            range: true,
-            min: $opts.min,
-            max: $opts.max,
-            values: $opts.values,
-            // slide event callback
-            slide: function( event, ui ) {
-              $.extend($opts, {include_left: event.keyCode != $.ui.keyCode.RIGHT});
-              $.extend($opts, {include_right: event.keyCode != $.ui.keyCode.LEFT}); 
-              // "Step" the slider to the next position
-              var current_value = $obj.findNearest(ui.value);
-              // find the true slider value
-              if (ui.value == ui.values[0]) {
-                  $obj.slider.slider('values', 0, current_value);
-              } else {
-                  $obj.slider.slider('values', 1, current_value);
-              }
-              // show the display slider value, with an array key lookup from display array
-              $obj.displaySliderValue();
-              return false;
-            }
-          });
-       }
-   };
+        }
+      }
+    }
+    
+    this.initSlider = function() {
+      $obj.slider = $elem.slider({
+        range: true,
+        min: $opts.min,
+        max: $opts.max,
+        values: $opts.values,
+        // slide event callback
+        slide: function( event, ui ) {
+          //store the keypress event for this slider and save it to the options
+          if(!$opts.include_left || !$opts.include_right) {
+            $.extend($opts, {include_left:  event.keyCode != $.ui.keyCode.RIGHT});
+            $.extend($opts, {include_right: event.keyCode != $.ui.keyCode.LEFT}); 
+          }
+          // "Step" the slider to the next position
+          var current_value = $obj.findNearest(ui.value);
+          // find the true slider value
+          if (ui.value == ui.values[0]) {
+            $obj.slider.slider('values', 0, current_value);
+          } else {
+            $obj.slider.slider('values', 1, current_value);
+          }
+          // show the display slider value, with an array key lookup from display array
+          $obj.displaySliderValue();
+          return false;
+        }
+      });
+    }
+  };
    
+   //Extend jQuery to add the steppedRangeSlider method
    $.fn.steppedRangeSlider = function(options)
    {
       $options = options;
